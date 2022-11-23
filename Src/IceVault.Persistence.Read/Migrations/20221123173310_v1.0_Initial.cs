aaff25ecf96e6bc2fe972e43aad340112205d0eb
Robbe Van Bael <robbe.vanbael@ravago.com>
2022-11-23 19:38:11 +0100

    Fix line endings

---
+++ Src/IceVault.Persistence.Read/Migrations/20221123173310_v1.0_Initial.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+
+#nullable disable
+
+namespace IceVault.Persistence.Read.Migrations
+{
+    [SuppressMessage("Usage", "EF1001:Internal EF Core API usage.")]
+    public partial class v10_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = migrationBuilder.ReadSql($"{this.GetId()}_Up");
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = migrationBuilder.ReadSql($"{this.GetId()}_Down");
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
